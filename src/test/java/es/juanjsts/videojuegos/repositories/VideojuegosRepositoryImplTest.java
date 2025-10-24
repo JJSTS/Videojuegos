ba902360376041d05f10120590e40495f2cf17eb
Juan <juanjesutrejosalinas06@gmail.com>
2025-10-24 11:53:33 +0200

    Creacion de VideojuegosRepositoryImplTest

--- src/test/java/es/juanjsts/videojuegos/repositories/VideojuegosRepositoryImplTest.java
+++ src/test/java/es/juanjsts/videojuegos/repositories/VideojuegosRepositoryImplTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -55,6 +56,7 @@ class VideojuegosRepositoryImplTest {
         repository = new VideojuegosRepositoryImpl();
         repository.save(videojuego1);
         repository.save(videojuego2);
+        repository.save(videojuego3);
     }
 
     @Test
@@ -99,37 +101,168 @@ class VideojuegosRepositoryImplTest {
 
     @Test
     void findAllByNombreAndGenero() {
+        String nombre = "Among us";
+        String genero = "Party";
+        List<Videojuego> videojuegos = repository.findAllByNombreAndGenero(nombre, genero);
+
+        //Assert
+        assertAll("findAllByNombreAndGenero",
+                () -> assertNotNull(videojuegos),
+                () -> assertEquals(1, videojuegos.size()),
+                () -> assertEquals(nombre, videojuegos.getFirst().getNombre()),
+                () -> assertEquals(genero, videojuegos.getFirst().getGenero())
+        );
     }
 
     @Test
-    void findById() {
+    void findById_existingId_returnOptionalWithVideojuego() {
+        Long id = 1L;
+        Optional<Videojuego> optionaVideojuego = repository.findById(id);
+
+        assertAll("findById_existingId_returnOptionalWithVideojuego",
+                () -> assertNotNull(optionaVideojuego),
+                () -> assertTrue(optionaVideojuego.isPresent()),
+                () -> assertEquals(id, optionaVideojuego.get().getId())
+        );
     }
 
     @Test
-    void findByUuid() {
+    void findById_nonExistingId_returnEmptyOptional() {
+        Long id = 100L;
+        Optional<Videojuego> optionaVideojuego = repository.findById(id);
+
+        assertAll("findById_nonExistingId_returnEmptyOptional",
+                () -> assertNotNull(optionaVideojuego),
+                () -> assertTrue(optionaVideojuego.isEmpty())
+        );
     }
 
     @Test
-    void existsById() {
+    void findByUuid_existingUuid_returnOptionalWithVideojuego() {
+        UUID uuid = videojuego1.getUuid();
+        Optional<Videojuego> optionaVideojuego = repository.findByUuid(uuid);
+
+        assertAll("findByUuid_existingUuid_returnOptionalWithVideojuego",
+                () -> assertNotNull(optionaVideojuego),
+                () -> assertTrue(optionaVideojuego.isPresent()),
+                () -> assertEquals(uuid, optionaVideojuego.get().getUuid())
+        );
     }
 
     @Test
-    void existsByUuid() {
+    void findByUuid_nonExistingUuid_returnEmptyOptional() {
+        UUID uuid = UUID.randomUUID();
+        Optional<Videojuego> optionaVideojuego = repository.findByUuid(uuid);
+
+        assertAll("findByUuid_nonExistingUuid_returnEmptyOptional",
+                () -> assertNotNull(optionaVideojuego),
+                () -> assertTrue(optionaVideojuego.isEmpty())
+        );
     }
 
     @Test
-    void save() {
+    void existsById_existingId_returnTrue() {
+        Long id = 1L;
+        boolean exists = repository.existsById(id);
+
+        assertTrue(exists);
+
     }
 
     @Test
-    void deleteById() {
+    void existsById_nonExistingId_returnFalse() {
+        Long id = 100L;
+        boolean exists = repository.existsById(id);
+
+        assertFalse(exists);
     }
 
     @Test
-    void deleteByUuid() {
+    void existsByUuid_existingUuid_returnTrue() {
+        UUID uuid = videojuego1.getUuid();
+        boolean exists = repository.existsByUuid(uuid);
+
+        assertTrue(exists);
+
+    }
+
+    @Test
+    void existsByUuid_nonExistingUuid_returnFalse() {
+        UUID uuid = UUID.randomUUID();
+        boolean exists = repository.existsByUuid(uuid);
+
+        assertFalse(exists);
+    }
+
+    @Test
+    void save_notExists() {
+        Videojuego videojuego = Videojuego.builder()
+                .id(4L)
+                .nombre("Valorant")
+                .genero("FPS")
+                .almacenamiento("10.0 GB")
+                .fechaDeCreacion(LocalDate.of(2020,12,12))
+                .costo(1.99)
+                .build();
+
+        Videojuego savedVideojuego = repository.save(videojuego);
+        var all = repository.findAll();
+
+        assertAll("save",
+                () -> assertNotNull(savedVideojuego),
+                () -> assertEquals(videojuego, savedVideojuego),
+                () -> assertEquals(4, all.size())
+        );
+    }
+
+    @Test
+    void save_butExists() {
+        Videojuego videojuego = Videojuego.builder().id(1L).build();
+
+        Videojuego savedVideojuego = repository.save(videojuego);
+        var all = repository.findAll();
+
+        assertAll("save",
+                () -> assertNotNull(savedVideojuego),
+                () -> assertEquals(videojuego, savedVideojuego),
+                () -> assertEquals(3, all.size())
+        );
+    }
+
+    @Test
+    void deleteById_existingId() {
+        Long id = 1L;
+        repository.deleteById(id);
+
+        var all = repository.findAll();
+
+        assertAll("deleteById_existingId",
+                () -> assertEquals(2, all.size()),
+                () -> assertFalse(repository.existsById(id))
+        );
+    }
+
+    @Test
+    void deleteByUuid_existingUuid() {
+        UUID uuid = videojuego1.getUuid();
+        repository.deleteByUuid(uuid);
+
+        var all = repository.findAll();
+
+        assertAll("deleteByUuid_existingUuid",
+                () -> assertEquals(2, all.size()),
+                () -> assertFalse(repository.existsByUuid(uuid))
+        );
     }
 
     @Test
     void nextId() {
+        Long nextId = repository.nextId();
+        var all = repository.findAll();
+
+        assertAll("nextId",
+                () -> assertEquals(4L, nextId),
+                () -> assertEquals(3, all.size())
+        );
     }
 }
