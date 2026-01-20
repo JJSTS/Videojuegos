5129edec65c63ea0544659b3b565cea08edf9261
JJSTS <juanjesustrejosalinas06@gmail.com>
2026-01-20 22:23:14 +0100

    Configuración en VideojuegosController

--- src/main/java/es/juanjsts/web/controller/VideojuegosController.java
+++ src/main/java/es/juanjsts/web/controller/VideojuegosController.java
@@ -1,13 +1,20 @@
 package es.juanjsts.web.controller;
 
+import es.juanjsts.rest.videojuegos.dto.VideojuegoCreateDto;
 import es.juanjsts.rest.videojuegos.dto.VideojuegoResponseDto;
 import es.juanjsts.rest.videojuegos.services.VideojuegosService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,5 +27,33 @@ public class VideojuegosController {
         VideojuegoResponseDto videojuego =  videojuegosService.findById(id);
         model.addAttribute("videojuego", videojuego);
         return "videojuegos/detalle";
+    }
+
+    @GetMapping({"","/","/lista"})
+    public String lista(Model model,
+                        @RequestParam(name = "page", defaultValue = "0") int page,
+                        @RequestParam(name = "size", defaultValue = "4") int size){
+        Pageable pageable = PageRequest.of(page, size, Sort.by("id").ascending());
+        Page<VideojuegoResponseDto> videojuegoPage = videojuegosService.findAll(
+                Optional.empty(), Optional.empty(),Optional.empty(), pageable);
+        model.addAttribute("page", videojuegoPage);
+        return "/videojuegos/lista";
+    }
+
+    @GetMapping("/new")
+    public String nuevoVideojuegoForm(Model model){
+        model.addAttribute("videojuego", VideojuegoCreateDto.builder().build());
+        return "/videojuegos/form";
+    }
+
+    @PostMapping("/new")
+    public String nuevoVideojuegoSubmit(@Valid @ModelAttribute VideojuegoCreateDto videojuego, BindingResult bindingResult){
+
+        if (bindingResult.hasErrors()){
+            return "/videojuegos/form";
+        } else {
+            videojuegosService.save(videojuego);
+            return "redirect:/videojuegos/lista";
+        }
     }
 }
